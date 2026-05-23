618fcaaf99aed5c694b0fae02bdcf9cb638c48fe
PORLA  MAHESH <maheshporla93@gmail.com>
2026-05-23 11:12:43 +0530

    ✅ Add solution for: Product Sales Analysis with Category Hierarchy - SQL

---
+++ exercises/ProductSalesAnalysisWithCategoryHierarchySQL.sql
@@ -0,0 +1,12 @@
+
+SELECT 
+    p.name AS product_name,
+    c.name AS category_name,
+    IFNULL(SUM(oi.quantity), 0) AS total_sold
+FROM products p
+LEFT JOIN categories c 
+    ON p.category_id = c.id
+LEFT JOIN order_items oi 
+    ON p.id = oi.product_id
+GROUP BY p.name, c.name
+ORDER BY total_sold DESC, product_name ASC
